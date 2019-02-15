9e77d6ade26149f892be78e166040a8cf64e0cfe
NiklasSorensen <niklass137@gmail.com>
2019-02-15 14:59:46 +0100

    Created the service

--- app/src/main/java/com/thegoodthebadtheasian/myapplication/MainActivity.java
+++ app/src/main/java/com/thegoodthebadtheasian/myapplication/MainActivity.java
@@ -1,13 +1,56 @@
 package com.thegoodthebadtheasian.myapplication;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RequestHandlerService mService;
+    boolean mBound = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        //Bind to the service
+        Intent intent = new Intent(this, RequestHandlerService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        unbindService(connection);
+    }
+
+
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            RequestHandlerService.LocalBinder binder = (RequestHandlerService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
 }
---
+++ app/src/main/java/com/thegoodthebadtheasian/myapplication/RequestHandlerService.java
@@ -0,0 +1,27 @@
+package com.thegoodthebadtheasian.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class RequestHandlerService extends Service {
+    //Binder to give to the clients
+    private final IBinder binder = new LocalBinder();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    public class LocalBinder extends Binder {
+        RequestHandlerService getService(){
+            //Return the instance of this service, so the client can call methods from the service
+            return RequestHandlerService.this;
+        }
+    }
+}
